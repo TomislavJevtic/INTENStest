115f26f2bc250d10f869503dc2e1082c6409f6fe
Darko <shibbytemplar@gmail.com>
2023-06-19 13:49:23 +0200

    prvi komit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("blaa");
+    }
+
+}
